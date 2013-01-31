5a95982c21df22d2a6e087a7b67ae3a866fd9298
AlexRNL <alexbarfety@free.fr>
2013-01-31 00:53:07 +0000

    Improve display of main (standard) charsets, to avoid errors.

--- src/org/clubrockisen/view/parameter/CharsetComboBox.java
+++ src/org/clubrockisen/view/parameter/CharsetComboBox.java
@@ -3,12 +3,18 @@ package org.clubrockisen.view.parameter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Logger;
 
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 
 import org.clubrockisen.service.abstracts.ParametersEnum;
+import org.clubrockisen.view.renderers.CharsetRenderer;
 
 /**
  * Combo box for the charset available.
@@ -17,6 +23,20 @@ import org.clubrockisen.service.abstracts.ParametersEnum;
 public class CharsetComboBox extends ParameterComponent {
 	/** Logger */
 	private static Logger		lg	= Logger.getLogger(CharsetComboBox.class.getName());
+	
+	/** The main char set (will be placed at the top of the list). */
+	private static Set<String>	mainCharset;
+	
+	/**
+	 * Load main char sets.
+	 */
+	static {
+		mainCharset = new HashSet<>();
+		mainCharset.add(StandardCharsets.ISO_8859_1.toString());
+		mainCharset.add(StandardCharsets.UTF_8.toString());
+		mainCharset.add(StandardCharsets.US_ASCII.toString());
+		mainCharset.add(StandardCharsets.UTF_16.toString());
+	}
 	
 	/** Combo box for the file charset available */
 	private JComboBox<String>	comboBox;
@@ -37,7 +57,28 @@ public class CharsetComboBox extends ParameterComponent {
 	@Override
 	public JComponent getComponent () {
 		if (comboBox == null) {
-			comboBox = new JComboBox<>(Charset.availableCharsets().keySet().toArray(new String[0]));
+			final TreeSet<String> charsets = new TreeSet<>(new Comparator<String>() {
+				@Override
+				public int compare (final String c1, final String c2) {
+					// If both strings are in (or out) of the main char sets
+					if ((mainCharset.contains(c1) && mainCharset.contains(c2)) ||
+							(!mainCharset.contains(c1) && !mainCharset.contains(c2))) {
+						return c1.compareTo(c2);
+					}
+					
+					// If the first string is in the main char set
+					if (mainCharset.contains(c1)) {
+						return -1;
+					}
+					
+					// Finally, the second string is in the main char set
+					return 1;
+				}
+				
+			});
+			charsets.addAll(Charset.availableCharsets().keySet());
+			comboBox = new JComboBox<>(charsets.toArray(new String[0]));
+			comboBox.setRenderer(new CharsetRenderer(mainCharset));
 		}
 		return comboBox;
 	}
