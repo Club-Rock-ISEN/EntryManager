0bfe968eeb39f1597b4adc5d46d4fbb279f6057f
Alex Barféty <alexbarfety@free.fr>
2014-01-16 00:35:36 +0100

    Revert "Move java source files to default folders."
    This reverts commit ad91810dd4dc76bf58779ca04eae756f246fb0c5.

--- src/main/java/org/clubrockisen/common/error/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Package for all top level error of the application.<br />
- * These should be caught in the application launcher.
- * @author Alex
- */
-package org.clubrockisen.common.error;
--- src/main/java/org/clubrockisen/view/renderers/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Package for the renderer of the component used in the GUI.
- * 
- * @author Alex
- */
-package org.clubrockisen.view.renderers;
