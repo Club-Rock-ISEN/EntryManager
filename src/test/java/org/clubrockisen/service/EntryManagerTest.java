6785234abb9ebba2a5b992b6b319cc039c1c3052
Alex Barféty <alexbarfety@free.fr>
2014-01-16 22:31:19 +0100

    Fix unit test of EntryManager#getPrice

--- src/test/java/org/clubrockisen/service/EntryManagerTest.java
+++ src/test/java/org/clubrockisen/service/EntryManagerTest.java
@@ -8,15 +8,21 @@ import java.nio.file.Paths;
 import org.clubrockisen.common.ConfigurationKeys;
 import org.clubrockisen.dao.abstracts.EntryManagerAbstractDAOFactory;
 import org.clubrockisen.entities.Member;
+import org.clubrockisen.entities.Parameter;
 import org.clubrockisen.entities.enums.Gender;
 import org.clubrockisen.entities.enums.Status;
 import org.clubrockisen.service.abstracts.IEntryManager;
+import org.clubrockisen.service.abstracts.IParametersManager;
+import org.clubrockisen.service.abstracts.ParametersEnum;
 import org.clubrockisen.service.abstracts.ServiceFactory;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.alexrnl.commons.database.dao.AbstractDAOFactory;
 import com.alexrnl.commons.database.dao.DataSourceConfiguration;
+import com.alexrnl.commons.time.Time;
 import com.alexrnl.commons.utils.Configuration;
 
 /**
@@ -24,14 +30,19 @@ import com.alexrnl.commons.utils.Configuration;
  * @author Alex
  */
 public class EntryManagerTest {
+	/** The parameter manager */
+	private static IParametersManager	pm;
+	/** The time limit previously set */
+	private static String				timeLimit;
+	
 	/** The entry manager to test */
-	private IEntryManager entryManager;
+	private IEntryManager				entryManager;
 	
 	/**
-	 * Set up test attributes.
+	 * Save the value of the time limit parameter.
 	 */
-	@Before
-	public void setUp () {
+	@BeforeClass
+	public static void setUpBeforeClass () {
 		final Configuration config = new Configuration(Paths.get(ConfigurationKeys.FILE));
 		final DataSourceConfiguration dbInfos = new DataSourceConfiguration(config.get(KEY.db().url()),
 				config.get(KEY.db().username()), config.get(KEY.db().password()),
@@ -39,6 +50,26 @@ public class EntryManagerTest {
 		final EntryManagerAbstractDAOFactory factory = AbstractDAOFactory.buildFactory(config.get(KEY.daoFactory()),
 				dbInfos, EntryManagerAbstractDAOFactory.class);
 		ServiceFactory.createFactory(config, factory);
+		
+		pm = ServiceFactory.getImplementation().getParameterManager();
+		timeLimit = pm.get(ParametersEnum.TIME_LIMIT).getValue();
+	}
+	
+	/**
+	 * Restore the value of the time limit parameter.
+	 */
+	@AfterClass
+	public static void tearDownAfterClass () {
+		final Parameter timeLimitParameter = pm.get(ParametersEnum.TIME_LIMIT);
+		timeLimitParameter.setValue(timeLimit);
+		pm.set(timeLimitParameter);
+	}
+	
+	/**
+	 * Set up test attributes.
+	 */
+	@Before
+	public void setUp () {
 		entryManager = ServiceFactory.getImplementation().getEntryManager();
 	}
 	
@@ -47,6 +78,10 @@ public class EntryManagerTest {
 	 */
 	@Test
 	public void testGetPrice () {
+		final Parameter timeLimitParameter = pm.get(ParametersEnum.TIME_LIMIT);
+		timeLimitParameter.setValue(Time.getCurrent().add(Time.get("00:10")).toString());
+		pm.set(timeLimitParameter);
+		
 		assertEquals(4.0, entryManager.getPrice(new Member(0, "Alex", Gender.MALE, 0, 0, 0.0, Status.MEMBER)), 0.01);
 		assertEquals(4.0, entryManager.getPrice(new Member(0, "Alex", Gender.MALE, 1, 0, 0.0, Status.MEMBER)), 0.01);
 		assertEquals(4.0, entryManager.getPrice(new Member(0, "Alex", Gender.MALE, 2, 0, 0.0, Status.MEMBER)), 0.01);
@@ -54,6 +89,10 @@ public class EntryManagerTest {
 		assertEquals(4.0, entryManager.getPrice(new Member(0, "Alex", Gender.MALE, 4, 0, 0.0, Status.MEMBER)), 0.01);
 		assertEquals(0.0, entryManager.getPrice(new Member(0, "Alex", Gender.MALE, 5, 0, 0.0, Status.MEMBER)), 0.01);
 		assertEquals(4.0, entryManager.getPrice(new Member(0, "Alex", Gender.MALE, 6, 0, 0.0, Status.MEMBER)), 0.01);
+		
+		timeLimitParameter.setValue(Time.getCurrent().sub(Time.get("00:10")).toString());
+		pm.set(timeLimitParameter);
+		assertEquals(3.0, entryManager.getPrice(new Member(0, "Alex", Gender.MALE, 0, 0, 0.0, Status.MEMBER)), 0.01);
 		
 		assertEquals(0.0, entryManager.getPrice(new Member(0, "Alex", Gender.MALE, 0, 0, 0.0, Status.HELPER_MEMBER)), 0.01);
 		assertEquals(0.0, entryManager.getPrice(new Member(0, "Alex", Gender.MALE, 1, 0, 0.0, Status.HELPER_MEMBER)), 0.01);
